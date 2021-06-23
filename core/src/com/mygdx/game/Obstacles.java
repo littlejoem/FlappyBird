d4f4d6e8998b57210172bddb2a160d6f02b1fa2e
littlejoem <malyshkz79@gmail.com>
2021-06-23 06:44:07 +0200

    Pair obstacles create

--- core/src/com/mygdx/game/Obstacles.java
+++ core/src/com/mygdx/game/Obstacles.java
@@ -4,19 +4,27 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.Random;
+
 public class Obstacles {
 
     class WallPair{
         Vector2 position;
         float speed;
+        int offset;
 
-        public WallPair(Vector2 pos){
+        public WallPair(Vector2 pos) {
             position = pos;
             speed = 2;
+            offset = new Random().nextInt(250);
         }
 
         public void update(){
             position.x -= speed;
+            if (position.x < -50) {
+                position.x = 800;
+                offset = new Random().nextInt(250);
+            }
         }
     }
 
@@ -37,7 +45,8 @@ public class Obstacles {
 
     public void render(SpriteBatch batch){
         for (int i = 0; i < obs.length; i++) {
-            batch.draw(tx, obs[i].position.x, obs[i].position.y);
+            batch.draw(tx, obs[i].position.x, obs[i].position.y - obs[i].offset);
+            batch.draw(tx, obs[i].position.x, obs[i].position.y + distance + tx.getHeight() - obs[i].offset);
         }
     }
 
